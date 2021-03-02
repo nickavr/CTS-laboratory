8bb6633d44f217a12a1c23ac34eee496cb278a8e
nickavr <nicuavram11@gmail.com>
2021-03-02 10:37:38 +0200

    First commit

---
+++ Seminar2/src/ro/ase/csie/cts/g1092/week2/exceptions/InsufficentFundsException.java
@@ -0,0 +1,13 @@
+package ro.ase.csie.cts.g1092.week2.exceptions;
+
+public class InsufficentFundsException extends Exception{
+
+    private String message;
+
+    public InsufficentFundsException(String msg){
+        super(msg); //call Exception constructor with the message
+    }
+
+    public InsufficentFundsException() {
+    }
+}
