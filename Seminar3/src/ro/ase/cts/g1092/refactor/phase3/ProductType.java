707f04cd1fa7d7408f12726b1e45be82957b7d27
nickavr <nicuavram11@gmail.com>
2021-03-09 11:33:08 +0200

    phase 3

---
+++ Seminar3/src/ro/ase/cts/g1092/refactor/phase3/ProductType.java
@@ -0,0 +1,14 @@
+package ro.ase.cts.g1092.refactor.phase3;
+
+public enum ProductType {
+    NEW(0), DISCOUNTED(0.1f), LIMITED_STOCK(0.25f), LEGACY(0.35f); //objects, not 0, 1, .. like in C
+    private float discount;
+
+    private ProductType(float discount) {
+        this.discount = discount;
+    }
+
+    public float getDiscount() {
+        return discount;
+    }
+}
---
+++ Seminar3/src/ro/ase/cts/g1092/refactor/phase3/SalesStrategy.java
@@ -0,0 +1,46 @@
+package ro.ase.cts.g1092.refactor.phase3;
+
+import ro.ase.cts.g1092.refactor.exceptions.InvalidValueException;
+
+public class SalesStrategy {
+    public static final int FIDELITY_YEARS_THRESHOLD = 10;
+    public static final float MAX_FIDELITY_DISCOUNT = 0.15f;
+
+    public static float getFidelityDiscont(int yearsSinceRegistration){
+        return  (yearsSinceRegistration > FIDELITY_YEARS_THRESHOLD) ?
+                MAX_FIDELITY_DISCOUNT : (float) yearsSinceRegistration /100;
+
+    }
+
+    public static float getPriceWithDiscount(float initialPrice, float discount, float fidelityDiscount){
+        return (initialPrice - (ProductType.DISCOUNTED.getDiscount() * initialPrice)) - fidelityDiscount * (initialPrice - (ProductType.DISCOUNTED.getDiscount() * initialPrice));
+    }
+
+    public float computeFinalPrice(float initialPrice, ProductType productType, int yearsSinceRegistration) throws InvalidValueException {
+        if(initialPrice <= 0 || yearsSinceRegistration < 0){
+            throw new InvalidValueException();
+        }
+
+        float finalPrice = 0;
+        float fidelityDiscount = getFidelityDiscont(yearsSinceRegistration);
+
+        switch (productType){
+            case NEW:
+                finalPrice = initialPrice;
+                break;
+            case DISCOUNTED:
+                finalPrice = computeFinalPrice(initialPrice, ProductType.DISCOUNTED, yearsSinceRegistration);
+                break;
+            case LIMITED_STOCK:
+                finalPrice = computeFinalPrice(initialPrice, ProductType.LIMITED_STOCK, yearsSinceRegistration);
+                break;
+            case LEGACY:
+                finalPrice = computeFinalPrice(initialPrice, ProductType.LEGACY, yearsSinceRegistration);
+                break;
+            default:
+                throw new UnsupportedOperationException("Type not managed");
+        }
+
+        return finalPrice;
+    }
+}
